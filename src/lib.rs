ab64c981aa7e7bf6b637441693c2637a482189ae
Nicolas REMOND <nicolas.remondn@thingthing.co>
2022-11-04 19:13:21 +0900

    ✅ Add unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
 pub fn is_odd(x: usize) -> bool {
     x % 2 == 1
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_odd() {
+        assert!(is_odd(1));
+    }
+
+    #[test]
+    fn test_even() {
+        assert!(!is_odd(2));
+    }
+
+    #[test]
+    fn test_zero() {
+        assert!(!is_odd(0));
+    }
+}
